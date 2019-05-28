4e1d4f404a2e068dbd909f22ffaa1d95aae11d53
ashea2120 <ashea2120@yahoo.com>
2019-05-28 08:37:42 -0400

    Task 1 adding a switch setting to app done

---
+++ app/src/main/java/com/example/appwithsettings/SettingsFragment.java
@@ -0,0 +1,21 @@
+package com.example.appwithsettings;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle
+                                            savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+}
